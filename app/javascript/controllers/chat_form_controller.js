b5ff59d3d25d897f004711c6065d727732c1e91e
Roberto de Farias <235542534+robertodefarias@users.noreply.github.com>
2026-03-17 19:06:53 -0300

    improve chat input loading state

--- app/javascript/controllers/chat_form_controller.js
+++ app/javascript/controllers/chat_form_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chat-form"
 export default class extends Controller {
+  static targets = ["input"]
+
   connect() {
+    this.defaultPlaceholder = "Pergunte algo sobre sua viagem..."
+    this.loadingPlaceholder = "Um momento por favor, estamos gerando seu roteiro QuickTrip..."
   }
 
-  static targets = ["input"]
-  reset() {
+  prepareForSubmit() {
     this.inputTarget.value = ""
+    this.inputTarget.placeholder = this.loadingPlaceholder
+    this.inputTarget.disabled = true
+  }
+
+  reset() {
+    this.inputTarget.disabled = false
+    this.inputTarget.value = ""
+    this.inputTarget.placeholder = this.defaultPlaceholder
     this.inputTarget.focus()
   }
 
